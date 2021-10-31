8b1bd87c5af9426fafedf8dc21773f0198df2534
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-10-31 15:37:56 -0400

    More skeleton WIP added to `main.py`.

--- asana_extensions/main.py
+++ asana_extensions/main.py
@@ -10,6 +10,8 @@ Module Attributes:
 (C) Copyright 2020 Jonathan Casey.  All Rights Reserved Worldwide.
 """
 import logging
+import signal
+import sys
 
 from asana_extensions.rules import rules
 
@@ -34,6 +36,37 @@ def main():
 
 
 
+def _setup_and_call_main():
+    """
+    Setup any pre-main operations, such as signals and input arg parsing, then
+    call `main()`.  This is basically what would normally be in
+    `if __name__ == '__main__':` prior to `main()` call, but this allows unit
+    testing a lot more easily.
+    """
+    for sig in ('INT', 'TERM', 'QUIT', 'HUP'):
+        signal.signal(getattr(signal, 'SIG%s' % sig), _shutdown)
+
+    main()
+
+
+
+def _shutdown(signum, _frame):
+    """
+    Perform all necessary operations to cleanly shutdown when required.
+
+    This is triggered through signal interrupts as registered when this is
+    executed as a script.
+
+    Args:
+      signum (int): Number of signal received.
+      _frame (frame): See signal.signal python docs.
+    """
+    msg = f'Exiting from signal {str(signum)} ...'
+    logger.warning(msg)
+    sys.exit(1)
+
+
+
 if __name__ == '__main__':                                  # Ignored by CodeCov
     # Since no unit testing here, code kept at absolute minimum
-    main()
+    _setup_and_call_main()
