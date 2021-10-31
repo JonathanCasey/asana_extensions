856d9e3ac8f4e9fd09c1ed6a4a5d74a2cd5d8ae8
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-10-31 13:59:50 -0400

    Unit test added for `execute_rules()`.  `fixture_blank_rule_cls()` moved to new `conftest.py` in `/tests/unit/rules` subpackage.

--- tests/unit/rules/test_rule_meta.py
+++ tests/unit/rules/test_rule_meta.py
@@ -27,48 +27,6 @@ from asana_extensions.rules import rule_meta
 
 
 
-@pytest.fixture(name='blank_rule_cls')
-def fixture_blank_rule_cls():
-    """
-    Returns a blank rule with default returns for all abstract methods.  This
-    can be used as is in most cases; in most other cases, this can serve as a
-    base with tests only needing to override individual methods via monkeypatch.
-    """
-    class BlankRule(rule_meta.Rule):
-        """
-        Simple blank rule to subclass Rule.
-        """
-        @classmethod
-        def load_specific_from_conf(cls, rules_cp, rule_id, rule_params=None,
-                **kwargs):
-            """
-            Not needed / will not be used.
-            """
-            return
-
-        @classmethod
-        def get_rule_type_names(cls):
-            """
-            Not needed / will not be used.
-            """
-            return []
-
-        def _sync_and_validate_with_api(self):
-            """
-            Not needed / will not be used.
-            """
-            return True
-
-        def execute(self, force_test_report_only=False):
-            """
-            Not needed / will not be used.
-            """
-            return True
-
-    return BlankRule
-
-
-
 def test_init(caplog, blank_rule_cls):
     """
     Tests `__init__()` method in `Rule`.
